174857104aecd26b20691206fd03a57b0e2a9719
pengxueshan <289159468@qq.com>
2016-02-26 16:54:10 +0800

    done

---
+++ public/js/app.js
@@ -0,0 +1,70 @@
+;$(function(){
+    //socket
+    (function(){
+        var socket = io();
+        var USERNAME;
+        var summary = $('.summary');
+
+        socket.on('update user', function (data) {
+            updateUser(data.username);
+        });
+
+        socket.on('login', function (data) {
+            if(data){
+                $('.login-layer').hide();
+                $('.users').append('<p><span>'+ data.username +' has login.</span></p>');
+            }
+        });
+
+        socket.on('update sum cache', function (data) {
+            if(data){
+                for(var key in data){
+                    updateSum(data[key]);
+                }
+            }
+        });
+
+        socket.on('update user cache', function (data) {
+            if(data){
+                data.forEach(function(ele, index){
+                    updateUser(ele);
+                });
+            }
+        });
+
+        socket.on('summary added', function (data) {
+            updateSum(data);
+        });
+
+        socket.on('update summary', function (data) {
+            updateSum(data);
+        });
+
+        function updateSum(data){
+            var sum = $('#' + data.id);
+            var html = '';
+            data.content.forEach(function(ele, index){
+                html += '<p>'+ ele +'</p>';
+            });
+            if(sum.length > 0){
+                sum.find('.c').html(html);
+            }else{
+                summary.append('<div class="item" id="'+ data.id +'"><p class="u">'+ data.id +'</p><div class="c">'+ html +'</div></div>');
+            }
+        }
+
+        function updateUser(username){
+            $('.users').append('<p><span>'+ username +' online.</span></p>');
+        }
+
+        $(document).on('click', '.login-layer .btn', function(){
+            USERNAME = $('.login-layer input').val();
+            socket.emit('add user', {username: USERNAME});
+        });
+
+        $(document).on('click', '.commit', function(){
+            var content = $('.content').val();
+            socket.emit('add summary', {id: USERNAME, content: content});
+        });
+    })();
+})
--- server.js
+++ server.js
@@ -5,30 +5,28 @@ var MASTER_KEY = process.env.LC_APP_MASTER_KEY;
 AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
 
 var express = require('express');
-// var cloud = require('./cloud');
-// var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
+var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
+
+var http = require('http');
+var io = require('socket.io');
 
 var app = express();
-// app.engine('handlebars', handlebars.engine);
-// app.set('views', 'views');
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars.engine);
+app.set('views', 'views');
+app.set('view engine', 'handlebars');
 
-// app.use(cloud);
 app.use(AV.Cloud);
 
 app.set('port', process.env.LC_APP_PORT || 3000);
 
-// app.use(express.static(__dirname + '/public'));
-// 
-// var links = ['www.ppp92.com','www.964vv.com','44bpbp.com','www.953vv.com','www.etet55.com','www.35ud.com','www.lujiulu.com','www.955ai.com','www.611hh.com','www.116ai.com','www.520gxgx.com','www.sdashao.com','953vv.com','www.sevip66.com','www.j80j90.com','www.927d.com','www.dedeshe.com','www.kang001.com','www.bu7777.com','www.123qxqx.com','www.ebeb33.com','17aaaa.com','www.88qqxx.com','etet55.com','77kgkg.com','www.zhibomo.com','www.r6297.com','611hh.com','www.17aaaa.com','www.44bpbp.com','www.mtlbbs.com','www.135ai.com','mtlbbs.com','www.sesezy88.com','www.55ggxx.com','www.73hw.com','www.o4764.com','www.44ggxx.com','www.123gxgx.com','964vv.com','bu7777.com','www.888gxgx.com','927d.com','ebeb33.com','www.890ai.com','www.55etet.com','sevip66.com','www.11wbwb.com','www.bbbbba.com','www.888hyhy.com','www.66ggxx.com','www.44apap.com','www.sesuse.com','www.66wbwb.com','www.66hyhy.com','www.lucaocao.com','zhibomo.com','www.66qqbb.com','sesezy88.com','88qqxx.com','www.77kgkg.com','55etet.com','www.88mcmc.com','sdashao.com','www.520hyhy.com','ppp92.com','www.55hyhy.com','www.9488i.com','888hyhy.com','www.j2jb.com','www.905x.com','44apap.com','888gxgx.com','44ggxx.com','520gxgx.com','135ai.com','123gxgx.com','520hyhy.com','66qqbb.com','88mcmc.com','66wbwb.com','116ai.com','66ggxx.com','j2jb.com','11wbwb.com','955ai.com','66hyhy.com','73hw.com','9488i.com','bbbbba.com','890ai.com'];
-// var links = [];
-// var html = '';
-// links.forEach(function(value, index){
-//     html += '<a href="http://'+ value +'">'+ value +'</a>';
-// });
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-    res.end('open');
+    res.render('home');
+});
+
+app.get('/thank-you', function(req, res){
+     res.end('thank you');
 });
 
 app.get('/getdata', function(req, res){
@@ -45,6 +43,53 @@ app.use(function(req, res){
     res.end('404 not found');
 });
 
-app.listen(app.get('port'), function(){
-    console.log('app started');
+var server = http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+io = io.listen(server);
+
+var cache = {};
+var users = [];
+
+io.on('connection', function(socket){
+    socket.on('add user', function (data) {
+        if(data){
+            users.push(data.username);
+            socket.emit('login', data);
+            socket.emit('update sum cache', cache);
+            socket.emit('update user cache', users);
+            socket.broadcast.emit('update user', data);
+        }
+    });
+
+    socket.on('add summary', function(data){
+        var id = data.id;
+        var content = data.content.replace(/\<|\>|\s/g, function(match){
+            switch(match){
+                case '<':
+                    return '&lt;';
+                    break;
+                case '>':
+                    return '&gt;';
+                    break;
+                case ' ':
+                    return '&nbsp;';
+                    break;
+                default:
+                    return match;
+                    break;
+            }
+        });
+        var c = content.split('\n');
+        var ret = {
+            id: data.id,
+            content: c
+        };
+        if(id && content){
+            cache[id] = ret;
+            socket.emit('summary added', ret);
+            socket.broadcast.emit('update summary', ret);
+        }
+    });
 });
